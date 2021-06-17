3855cd745e7bdebc18be8d47f70cb8e0b5b1aa7d
yayefall <cheikhibra1988@gmail.com>
2021-06-17 10:42:53 +0000

    pullll

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Affectation;
+use DateTime;
 use App\Repository\LitRepository;
 use App\Repository\EtudiantRepository;
 use PhpOffice\PhpSpreadsheet\Reader\Exception;
@@ -13,20 +14,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class EtudiantController extends AbstractController
 {
 
     private $etuRepo;
     private $litRep;
+    private $validator;
+    private $serializer;
 
 
-    public function  __construct(EtudiantRepository $etuRepo , LitRepository $litRep)
+    public function  __construct(EtudiantRepository $etuRepo,
+                                 LitRepository $litRep,
+                                 ValidatorInterface $validator,
+                                 SerializerInterface $serializer
+    )
 
     {
         $this->etuRepo = $etuRepo;
         $this->litRep = $litRep;
-
+        $this->validator = $validator;
+        $this->serializer = $serializer;
     }
     /**
      * @Route(
@@ -40,6 +50,9 @@ class EtudiantController extends AbstractController
 
     // importation fichier Excel
     public function import(Request $request){
+        $errors = [];
+        $annee = new DateTime;
+        $this->annee = $annee->format('Y');
 
           $doc = $request ->files->get('excelFile');
            		$file= IOFactory::identify($doc);
@@ -47,24 +60,30 @@ class EtudiantController extends AbstractController
            		$spreadsheet=$reader->load($doc);
            		$excel_file= $spreadsheet->getActivesheet()->toArray();
       	   // dd($excel_file);
-      	   for ($i = 1; $i < count($excel_file); $i++ ){
+      	   for ($i = 1; $i < count($excel_file); $i++ ) {
 
-               if (($etudiant = $this->etuRepo->findOneByNumero($excel_file[$i][0])) && ($lit = $this->litRep->findOneByNumero($excel_file[$i][4]))){
+               if (($etudiant = $this->etuRepo->findOneByNumero($excel_file[$i][0])) &&
+                   ($lit = $this->litRep->findOneByNumero($excel_file[$i][4]))) {
+                  // dd($etudiant);
                    $reservation = $etudiant->getReservation();
-                   foreach ($reservation as $rest){
-                       if ($rest->getAnnee()== '2021'){
+                   foreach ($reservation as $rest) {
+                       if ($rest->getAnnee() == $annee) {
                            $affection = new Affectation();
                            $affection->setReservation($rest);
                            $affection->setLit($lit);
                            dd($affection);
                        }
                    }
-
+               }elseif (!$etudiant && !$lit) {
+                   $error = "message d erreur";
+                   array_push($errors, $error);
                }
+             //  return new JsonResponse($errors, Response::HTTP_BAD_REQUEST);
 
            }
-       return new JsonResponse('l\'opération est traitée avec succes', Response::HTTP_OK);
+      return new JsonResponse('l\'opération est traitée avec succes', Response::HTTP_OK);
 
           }
+
 
 }
